1ca8e0994f7e1b6ba9583bee48921a9312ab7932
ludia39228 <71476418+ludia39228@users.noreply.github.com>
2021-03-12 11:30:24 +0800

    欄位程式碼ing

--- 2DPixelGame/Assets/Scripts/Player.cs
+++ 2DPixelGame/Assets/Scripts/Player.cs
@@ -3,8 +3,17 @@
 public class Player : MonoBehaviour
 {
     //修飾詞 類型 名稱(指定值);
-    public int Lv =1;
 
+
+    //類型 四大類型
+    //整數 int
+    //浮點數 float
+    //布林值 bool true是、false否
+    //字串 string
+    public int Lv = 1;
+    public float speed = 10.5f;
+    public bool isDead = false;
+    public string cName = "貓咪";
 
 
 }
