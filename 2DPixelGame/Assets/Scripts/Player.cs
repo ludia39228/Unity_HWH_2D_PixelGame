58774702e71fa17ef3296ec5d2e64c733dafaea2
ludia39228 <71476418+ludia39228@users.noreply.github.com>
2021-03-12 10:57:09 +0800

    語法ing

--- 2DPixelGame/Assets/Scripts/Player.cs
+++ 2DPixelGame/Assets/Scripts/Player.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //類型 名稱;
+    int level;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
