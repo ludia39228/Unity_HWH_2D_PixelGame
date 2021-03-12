0cca607453ee82e3ef2355dd27a81fd86c8d2925
ludia39228 <71476418+ludia39228@users.noreply.github.com>
2021-03-12 11:42:35 +0800

    inging...

--- 2DPixelGame/Assets/Scripts/Player.cs
+++ 2DPixelGame/Assets/Scripts/Player.cs
@@ -10,9 +10,12 @@ public class Player : MonoBehaviour
     //浮點數 float
     //布林值 bool true是、false否
     //字串 string
+    [Header("等級")]
     public int Lv = 1;
+    [Range(0,300)]
     public float speed = 10.5f;
     public bool isDead = false;
+    [Tooltip("這是角色的名稱")]
     public string cName = "貓咪";
 
 
