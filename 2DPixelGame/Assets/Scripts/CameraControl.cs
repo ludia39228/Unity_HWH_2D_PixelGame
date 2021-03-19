826027c1635e2ca565d227bf9d2075e5887bcf73
ludia39228 <71476418+ludia39228@users.noreply.github.com>
2021-03-19 11:54:19 +0800

    對很好最好不要跳掉

---
+++ 2DPixelGame/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("追蹤速度"), Range(0, 50)]
+    public float speed=1.5f;
+    [Header("上下邊界")]
+    public Vector2 limitＹ = new Vector2(-5, 5);
+    [Header("左右邊界")]
+    public Vector2 limitＸ = new Vector2(-5, 5);
+
+}
