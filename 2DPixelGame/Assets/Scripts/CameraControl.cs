603cb6cb3661f80d1eee1c9488310e638d5667cb
ludia39228 <ludia39228@gmail.com>
2021-03-26 09:31:12 +0800

    電腦當了4次才終於趕上的進度

--- 2DPixelGame/Assets/Scripts/CameraControl.cs
+++ 2DPixelGame/Assets/Scripts/CameraControl.cs
@@ -8,5 +8,29 @@ public class CameraControl : MonoBehaviour
     public Vector2 limitＹ = new Vector2(-5, 5);
     [Header("左右邊界")]
     public Vector2 limitＸ = new Vector2(-5, 5);
+    [Header("玩家")]
+    public Transform player;
+
+    
+
+    private void Update()
+    {
+        Track();
+    }
+
+    ///<summary>
+    ///追蹤
+    ///</summary>
+    
+    private void Track()
+    {
+        Vector3 posCam = transform.position;   //取得 攝影機座標
+        Vector3 posPla = player.position;      //取得 玩家座標
+
+        posCam = Vector3.Lerp(posCam, posPla, 0.5f);
+
+        print(posCam);
+
+    }
 
 }
