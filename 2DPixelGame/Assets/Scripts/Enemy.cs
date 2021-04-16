1e557f7232c1c432f0bb737a6d1da2467f5ceed4
ludia39228 <ludia39228@gmail.com>
2021-04-16 10:06:48 +0800

    怪物追蹤

---
+++ 2DPixelGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("追蹤範圍"), Range(0, 500)]
+    public float rangeTrack = 2;
+
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.Find("人物").transform;
+    }
+
+    //繪製圖示事件:在Unity內顯示輔助開發
+    private void OnDrawGizmos()
+    {
+        //先指定顏色再畫圖
+        Gizmos.color = new Color(0, 0, 1, 0.35f);
+        //繪製球體(中心點,半徑)
+        Gizmos.DrawSphere(transform.position, rangeTrack);
+
+
+    }
+
+    private void Update()
+    {
+        Track();
+    }
+
+    /// <summary>
+    /// 追蹤玩家
+    /// </summary>
+    private void Track()
+    {
+        //距離 等於 三維向量 的 距離(A點,B點)
+        float dis = Vector3.Distance(transform.position, player.position);
+
+        //如果 距離 小於等於 追蹤範圍 才開始追蹤
+        if(dis<=rangeTrack)
+        {
+            print("追蹤");
+        }
+    }
+
+}
