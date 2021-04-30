dd29914ff5c12677153b00b309e7d89de82a5c6b
ludia39228 <71476418+ludia39228@users.noreply.github.com>
2021-04-30 11:40:24 +0800

    貼貼

--- 2DPixelGame/Assets/Scripts/HpManager.cs
+++ 2DPixelGame/Assets/Scripts/HpManager.cs
@@ -23,6 +23,15 @@ public class HpManager : MonoBehaviour
     {
         RectTransform rect = Instantiate(rectDamage, transform);   //生成傷害數值在血條系統內
         rect.anchoredPosition = new Vector2(0, 200);               //指定座標
+
+        float y = rect.anchoredPosition.y;                         //取得原始Y軸
+
+        while(y<400)                                               //當Y小於400時持續執行
+        {
+            y += 20;                                               //每次上升的單位20
+            rect.anchoredPosition= new Vector2(0, y);              //更新座標
+            yield return new WaitForSeconds(0.02f);                //等待幾秒(秒數)
+        }
     }    
 
 }
