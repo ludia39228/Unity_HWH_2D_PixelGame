822d31cbdd9269d740751b699ad260f7c34372f1
ludia39228 <ludia39228@gmail.com>
2021-04-30 09:04:12 +0800

    程式碼程式碼

--- 2DPixelGame/Assets/Scripts/HpManager.cs
+++ 2DPixelGame/Assets/Scripts/HpManager.cs
@@ -5,4 +5,14 @@ public class HpManager : MonoBehaviour
 {
     [Header("血條")]
     public Image bar;
+
+    /// <summary>
+    /// 輸入血量與血量最大值並更新血條
+    /// </summary>
+    /// <param name="hp">當前血量</param>
+    /// <param name="hpBar">血量最大值</param>
+    public void UpdateHpBar(float hp,float hpBar)
+    {
+        
+    }
 }
