5d48b2822fc299dfb27abfbb7d4a4c7e161a61ff
ludia39228 <ludia39228@gmail.com>
2021-05-14 10:00:58 +0800

    陣列

--- 2DPixelGame/Assets/Scripts/ExpData.cs
+++ 2DPixelGame/Assets/Scripts/ExpData.cs
@@ -3,5 +3,9 @@
 [CreateAssetMenu(fileName ="經驗值資料",menuName ="DNF/經驗值資料" )]
 public class ExpData : ScriptableObject
 {
-
+    //陣列
+    //語法：在類型後面加上一對中括號
+    //陣列的作用：儲存多筆相同類型的資料
+    [Header("每個等級經驗值需求,從一等開始")]
+    public float[] exp;
 }
