e4984518971d6668c220a7c52eea10ddd3c79ac7
ludia39228 <71476418+ludia39228@users.noreply.github.com>
2021-05-21 09:33:37 +0800

    詭異的程式碼

---
+++ 2DPixelGame/Assets/Scripts/NPC.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("商店介面")]
+    public GameObject objShop;
+
+    /// <summary>
+    /// 開啟商店介面
+    /// </summary> 
+    public void OpenShop();
+    {
+        objShop.SetActive(true);
+    }
+/// <summary>
+/// 關閉商店介面
+/// </summary>
+  public void CloseShop();
+  {
+    objShop.SetActive(false);
+}
+
+
+}
+    
+
